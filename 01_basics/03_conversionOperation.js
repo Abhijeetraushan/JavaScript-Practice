7621f65daebcde68b0701827e4f13dde6d48d9ff
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-28 19:33:53 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score="33aabbcc"
+
+console.log(typeof(score));
+
+let FinalScore=Number(score);
+console.log(FinalScore)
+
+//"33" = number
+//33aa =NaN (not a number)
+// true / false = 1 / 0
+
+let isLogIn =14
+
+console.log(typeof isLogIn)
+
+let loggedIn =Boolean(isLogIn)
+console.log(loggedIn)
+console.log(typeof loggedIn)
