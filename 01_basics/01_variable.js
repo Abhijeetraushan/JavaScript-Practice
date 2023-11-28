c47ba861e2ba05647b5d3ae5b90e1b5011eb88ae
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-28 19:14:30 +0000

    variable and data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const account_no=555
+let name="Abhijeet"
+var reg_no=11715660
+stream = "computer science"
+
+/* we should prefer let as var does not have block or functional scope*/
+ console.table([account_no,name,reg_no,stream])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat js code as newer version
+
+//javascript has 7 data types
+/*
+string
+symbol
+number
+null
+bigint
+boolean 
+undefined
+*/
+
+
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; // foo is now a boolean
+console.log(foo)
