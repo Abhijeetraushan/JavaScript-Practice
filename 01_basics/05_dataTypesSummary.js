b7afd6653b2497ca20d5bc864f898b222f60b9e9
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-29 04:03:39 +0000

    heap and sack memory allocation

--- 01_basics/05_dataTypesSummary.js
+++ 01_basics/05_dataTypesSummary.js
@@ -35,3 +35,31 @@ console.log(typeof myFun)
 console.log(typeof myObj)
 console.log(typeof arr)
 myFun()
+
+
+
+//********************memory**************************
+//stack(primitive)
+//heap(non-primitive)
+
+let num1 =14562
+let num2=num1
+num2=654
+
+console.log(num1) 
+console.log(num2)
+// above code uses stack and get another copy ...so changes made in another won't reflect in previous data types
+
+
+const myob={
+    name:"abhi",
+    roll:45,
+    class:"X"
+}
+
+const myob2=myob
+myob2.name="monu"
+console.log(myob)
+console.log(myob2)
+/*in above code when we assign myob to myob2 than it get reference to same heap and 
+changes made in second object reflect in first object*/
