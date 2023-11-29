0a209a197c30dee87ca992b0bff47c8feaa5775b
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-29 05:16:26 +0000

    math and number

---
+++ 01_basics/07_numAndMath.js
@@ -0,0 +1,14 @@
+const score=215
+console.log(score)
+
+const avg=new Number(45) //explicitly defining number data types
+console.log(avg)
+console.log(avg.toString().length)
+
+
+//***********Maths***************
+console.log(Math.ceil(4.596))
+console.log(Math.floor(5.963))
+console.log(Math.sqrt(5))
+console.log(Math.floor(Math.random()*10+1))
+
