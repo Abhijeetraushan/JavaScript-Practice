744ede45a8a9a8148112525eed8b23e160862c68
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-29 04:47:39 +0000

    practiced string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,13 @@
+name="Abhijeet"
+repoCount=10
+
+console.log("my name is "+name+ " and my repo count is "+repoCount)
+//above line of code is discouraged and is outdated
+
+// string interpolation is used and encouraged
+console.log(`my name is ${name} and my repo count is ${repoCount}`)
+
+const myname=new String("Abhijeet")
+
+console.log(myname.length)
+console.log(myname.toLocaleUpperCase())
