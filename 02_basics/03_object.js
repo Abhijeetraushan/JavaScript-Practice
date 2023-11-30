cfaca6a8cdfcb81e7bb3252b8af62b35510dc235
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-30 16:47:55 +0000

    function

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -12,11 +12,11 @@ const JsUsers={
     ['Programming Language']:"JavaScript",
     e_mail:"abhijeet@gmail.com"
 }
-/* console.log(JsUsers["Programming Language"])
+//console.log(JsUsers["Programming Language"])
 console.log(JsUsers[MySymbol])
- */
+ 
 JsUsers.e_mail="monushahi@google.com"
-console.log(JsUsers.e_mail)
+//console.log(JsUsers.e_mail)
 //Object.freeze(JsUsers)
 JsUsers.e_mail="sonushahi@google.com"
 //console.log(JsUsers.e_mail)
@@ -28,5 +28,5 @@ JsUsers.greeting=function(){
 JsUsers.greeting2=function(){
     console.log(`hello ${this.name} ,how are you doing`)
 }
-console.log(JsUsers.greeting())
-console.log(JsUsers.greeting2())
+//console.log(JsUsers.greeting())
+//console.log(JsUsers.greeting2())
