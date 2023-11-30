ba23f61a7ee84128150ac0501ee7c1cf47cba896
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-11-30 16:44:37 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,7 @@
+function printSomething(){
+    console.log("function is invoked ")
+    console.log("function is executed")
+}
+
+printSomething() // function call
+printSomething //return only function reference
