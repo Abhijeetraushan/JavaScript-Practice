a324822748a5d132f35588f0fb238cba19a9a174
Abhijeetraushan <43603615+Abhijeetraushan@users.noreply.github.com>
2023-12-01 01:09:41 +0000

    ForEach

---
+++ Iteration/ForEach.js
@@ -0,0 +1,20 @@
+//let ar=["c++","c","Python","Java","R","Javascript","C#"]
+//ar.forEach((item)=>{console.log(item)})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+ myCoding.forEach((item)=>{
+    console.log(item.languageFileName)
+ })
